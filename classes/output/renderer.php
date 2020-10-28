8b39b97243d70d3eb2d8719dad373eb46241bb9a
justinhunt <bitwalkerjapan@gmail.com>
2020-10-28 15:57:39 +0900

    i

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -225,7 +225,7 @@ class renderer extends \plugin_renderer_base {
         $results = array_filter($steps, function($step){return $step->hasgrade;});
         $useresults=[];
         foreach($results as $result){
-            $result->title=$quizdata[$result->index]->title;
+            $result->title=$quizdata[$result->index]->name;
             $result->index++;
             $useresults[]=$result;
         }
