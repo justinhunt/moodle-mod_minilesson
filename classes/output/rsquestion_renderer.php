e845167d48e83d038c93a424d7a185cf7ddeabcb
justinhunt <bitwalkerjapan@gmail.com>
2023-04-25 01:55:34 +0000

    made gapfills public

--- classes/output/rsquestion_renderer.php
+++ classes/output/rsquestion_renderer.php
@@ -49,11 +49,11 @@ class rsquestion_renderer extends \plugin_renderer_base {
                 constants::TYPE_DICTATION,constants::TYPE_SPEECHCARDS, constants::TYPE_LISTENREPEAT];
         $qtypes[]= constants::TYPE_MULTIAUDIO;
         $qtypes[]=constants::TYPE_SHORTANSWER;
+     $qtypes[]=constants::TYPE_LGAPFILL;
+     $qtypes[]=constants::TYPE_TGAPFILL;
+     $qtypes[]=constants::TYPE_SGAPFILL;
         if(isset($CFG->minilesson_experimental) &&$CFG->minilesson_experimental){
            $qtypes[]=constants::TYPE_SMARTFRAME;
-            $qtypes[]=constants::TYPE_LGAPFILL;
-            $qtypes[]=constants::TYPE_TGAPFILL;
-            $qtypes[]=constants::TYPE_SGAPFILL;
             $qtypes[]=constants::TYPE_COMPQUIZ;
             $qtypes[]=constants::TYPE_BUTTONQUIZ;
         }
