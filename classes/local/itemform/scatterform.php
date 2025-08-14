61902508016816f997836a156d26b247981f3d46
rajuptl21 <rajuptl21@gmail.com>
2025-08-14 15:03:39 +0530

    scatter lesson item

--- classes/local/itemform/scatterform.php
+++ classes/local/itemform/scatterform.php
@@ -24,6 +24,8 @@ class scatterform extends baseform
         get_string('enterscatteritems', constants::M_COMPONENT));
         $this->add_textarearesponse(1, get_string('scatteritems',  constants::M_COMPONENT), true);
         $this->add_allowretry(constants::SCATTER_ALLOWRETRY, '', 1);
+
+        $this->add_timelimit(constants::TIMELIMIT, get_string(constants::TIMELIMIT, constants::M_COMPONENT));
     }
 
 }
