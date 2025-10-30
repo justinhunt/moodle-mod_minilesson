5bb288575c1b5035627be31cb7ef094bb9497aba
Raju Patel <rajuptl21@gmail.com>
2025-10-30 16:26:28 +0530

    slides lesson item

--- amd/src/slides.js
+++ amd/src/slides.js
@@ -1,52 +1,105 @@
-define(['jquery', 'core/log', 'mod_minilesson/definitions'],
-    function($, log, def, anim) {
-  "use strict"; // jshint ;_;
+define([
+    'jquery', 'core/log', 'mod_minilesson/definitions', './reveal',
+    'core/str', 'core/modal_factory', 'core/fragment'
+],
+    function ($, log, def, RevealImplement, Str, ModalFactory, Fragment) {
+        "use strict"; // jshint ;_;
 
-   /*
-  This file is to manage the slides item type
-   */
+        /*
+       This file is to manage the slides item type
+        */
 
-  log.debug('MiniLesson Slides: initialising');
+        log.debug('MiniLesson Slides: initialising');
 
-  return {
+        return {
 
-      //for making multiple instances
-      clone: function () {
-          return $.extend(true, {}, this);
-      },
+            instance: null,
 
-      init: function(index, itemdata, quizhelper) {
-      this.register_events(index, itemdata, quizhelper);
-    },
+            itemdata: {},
 
-    prepare_html: function(itemdata) {
-      //do something
-    },
+            //for making multiple instances
+            clone: function () {
+                return $.extend(true, {}, this);
+            },
 
-    register_events: function(index, itemdata, quizhelper) {
-      //When click next button , report and leave it up to parent to eal with it.
-      $("#" + itemdata.uniqueid + "_container .minilesson_nextbutton").on('click', function(e) {
-        var stepdata = {};
-          stepdata.index = index;
-          stepdata.hasgrade = false;
-          stepdata.totalitems=0;
-          stepdata.correctitems=0;
-          stepdata.grade = 0;
-        quizhelper.do_next(stepdata);
-      });
-      $("#" + itemdata.uniqueid + "_container").on("showElement", () => {
-          if (itemdata.timelimit > 0) {
-            $("#" + itemdata.uniqueid + "_container .progress-container").show();
-            $("#" + itemdata.uniqueid + "_container .progress-container i").show();
-            $("#" + itemdata.uniqueid + "_container .progress-container #progresstimer").progressTimer({
-                height: '5px',
-                timeLimit: itemdata.timelimit,
-                onFinish: function() {
-                    $("#" + itemdata.uniqueid + "_container .minilesson_nextbutton").trigger('click');
+            init: function (index, itemdata, quizhelper) {
+                this.itemdata = itemdata;
+                this.register_events(index, itemdata, quizhelper);
+            },
+
+            prepare_html: function (itemdata) {
+                //do something
+            },
+
+            register_events: function (index, itemdata, quizhelper) {
+                var self = this;
+                //When click next button , report and leave it up to parent to eal with it.
+                $("#" + itemdata.uniqueid + "_container .minilesson_nextbutton").on('click', function (e) {
+                    var stepdata = {};
+                    stepdata.index = index;
+                    stepdata.hasgrade = false;
+                    stepdata.totalitems = 0;
+                    stepdata.correctitems = 0;
+                    stepdata.grade = 0;
+                    quizhelper.do_next(stepdata);
+                });
+                $("#" + itemdata.uniqueid + "_container").on("showElement", async (e) => {
+                    if (!self.instance) {
+                        self.instance = await RevealImplement.init(e.target.querySelector('.reveal'));
+                        self.instance.initialize();
+                    }
+                    RevealImplement.setTheme(self.itemdata.selectedtheme);
+                    if (itemdata.timelimit > 0) {
+                        $("#" + itemdata.uniqueid + "_container .progress-container").show();
+                        $("#" + itemdata.uniqueid + "_container .progress-container i").show();
+                        $("#" + itemdata.uniqueid + "_container .progress-container #progresstimer").progressTimer({
+                            height: '5px',
+                            timeLimit: itemdata.timelimit,
+                            onFinish: function () {
+                                $("#" + itemdata.uniqueid + "_container .minilesson_nextbutton").trigger('click');
+                            }
+                        });
+                    }
+                });
+            },
+
+            register_previewbutton: function (buttonid) {
+                var previewbtn = document.getElementById(buttonid);
+                if (!previewbtn) {
+                    return;
                 }
-            });
-          }
-      });
-    }
-  }; //end of return value
-});
+                previewbtn.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    var form = previewbtn.form;
+                    if (!form) {
+                        return;
+                    }
+                    ModalFactory.create({
+                        type: ModalFactory.types.CANCEL,
+                        large: true,
+                        removeOnClose: true,
+                        templateContext: {
+                            classes: 'minilesson-slides-preview-modal'
+                        },
+                        title: Str.get_string('slides:previewmodaltitle', 'mod_minilesson'),
+                        body: Fragment.loadFragment('mod_minilesson', 'preview_slides', M.cfg.contextid, {
+                            formdata: new URLSearchParams([...new FormData(form).entries()]).toString()
+                        })
+                    }).then(function (modal) {
+                        modal.getFooter().addClass('d-none');
+                        modal.show();
+                    });
+                    return;
+                });
+                if (previewbtn.form) {
+                    const themeselect = previewbtn.form.querySelector('[data-control="theme"]');
+                    if (themeselect) {
+                        themeselect.addEventListener('change', function(e) {
+                            RevealImplement.setTheme(e.target.value);
+                        });
+                        RevealImplement.setTheme(themeselect.value);
+                    }
+                }
+            }
+        }; //end of return value
+    });
--- classes/local/itemform/slidesform.php
+++ classes/local/itemform/slidesform.php
@@ -15,13 +15,59 @@ class slidesform extends baseform
 {
     public $type = constants::TYPE_SLIDES;
 
+    // Got list from https://api.github.com/repos/hakimel/reveal.js/contents/css/theme/source?ref=5.2.1
+    const THEMES = [
+        'beige',
+        'black',
+        'black-contrast',
+        'blood',
+        'dracula',
+        'league',
+        'moon',
+        'night',
+        'serif',
+        'simple',
+        'sky',
+        'solarized',
+        'white',
+        'white_contrast_compact_verbatim_headers',
+        'white-contrast',
+    ];
+
     /**
      * Add any form fields specific to this item type.
      */
     public function custom_definition() {
+        global $PAGE;
         $this->add_itemsettings_heading();
         $mform = $this->_form;
         $this->add_static_text('instructions', '', get_string('enterslidesmarkdown', constants::M_COMPONENT));
         $this->add_textarearesponse(constants::SLIDES_MARKDOWN, get_string('slidesmarkdown', constants::M_COMPONENT), true);
+
+        $this->add_media_upload(constants::FILEANSWER . '1', get_string('slides:attachments', constants::M_COMPONENT), false, 'image,audio,video', -1);
+
+        $themeoptions = array_combine(self::THEMES, self::THEMES);
+        $mform->addElement('select', constants::SLIDETHEME, get_string('slides:theme', constants::M_COMPONENT), $themeoptions, ['data-control' => 'theme']);
+        $mform->setType(constants::SLIDETHEME, PARAM_ALPHA);
+
+        $emtopx = 16;
+        $fontsizes[1.8 * $emtopx] = get_string('slides:fontsmall', constants::M_COMPONENT);
+        $fontsizes[1.6 * $emtopx] = get_string('slides:fontsmaller', constants::M_COMPONENT);
+        $fontsizes[2.0 * $emtopx] = get_string('slides:fontstandard', constants::M_COMPONENT);
+        $fontsizes[2.4 * $emtopx] = get_string('slides:fontlarger', constants::M_COMPONENT);
+        $fontsizes[2.2 * $emtopx] = get_string('slides:fontlarge', constants::M_COMPONENT);
+        $mform->addElement('select', constants::SLIDEFONTSIZE, get_string('slides:fontsize', constants::M_COMPONENT), $fontsizes);
+        $mform->setType(constants::SLIDEFONTSIZE, PARAM_FLOAT);
+        $mform->setDefault(constants::SLIDEFONTSIZE, 2.0 * $emtopx);
+
+        $mform->registerNoSubmitButton('previewbutton');
+        $previewbtn = $mform->addElement('submit', 'previewbutton', get_string('slides:preview', constants::M_COMPONENT));
+        $previewbtn->_generateId();
+        $previewbtn->updateAttributes(['id' => $previewbtn->getAttribute('id') . '_' . random_string()]);
+        $PAGE->requires->js_call_amd(
+            constants::M_COMPONENT . '/slides',
+            'register_previewbutton',
+            [$previewbtn->getAttribute('id')
+        ]);
     }
 }
