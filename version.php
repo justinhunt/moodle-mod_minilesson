91dd9b00b9ec160299d6126e937673972122629a
Justin Hunt <bitwalkerjapan@gmail.com>
2026-01-21 09:10:25 +0900

    version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2026013001;
+$plugin->version   = 2026013002;
 $plugin->requires  = 2016052300;      // Requires Moodle 3.1.
 $plugin->component = 'mod_minilesson';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'Version 1.1.21 (Build 2026013001)';
+$plugin->release = 'Version 1.1.22 (Build 2026013002)';
